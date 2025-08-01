f173788c31b947bc4f8fdfcf317e7f376cc2c328
UtkarshTrivedi06 <utkarshtrivedi2006@gmail.com>
2025-08-01 10:01:15 +0530

    Time: 0 ms (100%), Space: 19.8 MB (21.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,18 +1,11 @@
-void moveZeroes(int* a, int c) {
-    int cnt=0,i=0,j;
-    while(i<c-cnt){
-        if(a[i]==0){
-            int t=a[i];
-            for(j=i;j<c-cnt-1;j++){
-                a[j]=a[j+1];
-                
-            }
-            a[j]=t;
-            cnt++;
-        }
-        else{
-            i++;
+void moveZeroes(int* nums, int n) {
+    int c=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]!=0){
+            int t=nums[i];
+            nums[i]=nums[c];
+            nums[c]=t;
+            c++;
         }
     }
-        
 }
