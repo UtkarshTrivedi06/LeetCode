f44744da4a8ee40ba31456dc7b0be93a341697fa
UtkarshTrivedi06 <utkarshtrivedi2006@gmail.com>
2025-08-08 22:02:14 +0530

    Time: 1 ms (41.33%), Space: 13.9 MB (71.01%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.c
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -1,17 +1,6 @@
 int maxArea(int* height, int heightSize) {
     int m=0,h=0;
-    /*for(int i=0;i<heightSize-1;i++){
-        for(int j=i+1;j<heightSize;j++){
-            h=pow(pow(height[i]-height[j],2),0.5);
-            if((height[i]-h)*(j-i)>m&&height[j]<=height[i]){
-                m=(height[i]-h)*(j-i);
-            }else if((height[j]-h)*(j-i)>m&&height[i]<height[j]){
-                m=(height[j]-h)*(j-i);
-            }
-        }
-    }*/
     int left = 0, right = heightSize - 1;
-
     while (left < right) {
         int h = height[left] < height[right] ? height[left] : height[right];
         int area = h * (right - left);
